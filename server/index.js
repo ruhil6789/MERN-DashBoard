894cf51e44f8beada7f4b43e48d21509bbeee792
sachinruhil11 <sachinruhil6789@gmail.com>
2023-04-01 18:18:25 +0530

    issue in package

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import clientRoutes from "./routes/client.js";
+import salesRoutes from "./routes/sales.js";
+import managementRoutes from "./routes/management.js";
+import generalRoutes from "./routes/general.js";
+
+
+/*  configuration*/
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })) // sharing request
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+/* setup routes*/
+app.use("/client", clientRoutes)
+app.use("/general", generalRoutes)
+app.use("/management", managementRoutes)
+app.use("/sales", salesRoutes)
+
+
+
+
